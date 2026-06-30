346345668919998b87964efde71f0c2088ce1fe8
Thiviru Malitha <malitha929@gmail.com>
2026-07-01 01:23:43 +0530

    Add files via upload

---
+++ Simulation/code.py
@@ -0,0 +1,31 @@
+pi = 22/7
+
+f_0 = 1000
+
+C = 1*10**(-6)
+
+k = 2*pi*f_0*C
+
+R = 1/k
+
+R_1 = R
+R_2 = R
+R_3 = R/2
+
+C_1 = C
+C_2 = C
+C_3 = 2*C
+
+print("R_1")
+print (R_1)
+print("R_2")
+print (R_2)
+print("R_3")
+print (R_3)
+
+print("C_1")
+print (C_1)
+print("C_2")
+print (C_2)
+print("C_3")
+print (C_3)
